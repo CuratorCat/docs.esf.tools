37da740c7e844b3dd531517c966b665e8c360031
curatorcat <102763039+curatorcat@users.noreply.github.com>
2022-06-28 12:35:01 -0700

    update home page

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,57 +1,57 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
+import Link from "@docusaurus/Link";
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  link: string;
+  // Svg: React.ComponentType<React.ComponentProps<"svg">>;
+  icon: string;
   description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: "Guides",
+    link: "/guides",
+    // Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
+    icon: "📖",
     description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
+      <>Learn how to use all the features of ENS Subdomain Factory.</>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
+    title: "FAQs",
+    link: "/guides/faqs",
+    // Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
+    icon: "🙋‍♂️",
+    description: <>Frequently Asked Questions about ENS Subdomain Factory.</>,
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: "Developers",
+    link: "/developers",
+    // Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
+    icon: "🤖",
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Developer documentation for ENS Subdomain Factory.
       </>
     ),
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, description, icon, link }: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+    <Link to={link} className={clsx("col col--4", styles.featureItemLink)}>
+      <div className={styles.featureItem}>
+        <div className="text--center">
+          <p className={styles.featureIcon}>{icon}</p>
+          <h3>{title}</h3>
+          <div><span>{description}</span></div>
+        </div>
       </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
+    </Link>
   );
 }
 
